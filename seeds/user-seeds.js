e037f21ac43d2990906d8fad830c1cec7afe57a7
RCSchornick <94027300+RCSchornick@users.noreply.github.com>
2022-03-13 21:43:14 -0500

    finishing module 14.5.7

---
+++ seeds/user-seeds.js
@@ -0,0 +1,59 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userdata = [
+  {
+    username: 'mikejtb66',
+    email: 'mikeistheboss@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'sandraeb62',
+    email: 'ilovepiano@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'lukeimnotyourfather29',
+    email: 'vadersucks@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'andykin11',
+    email: 'padmeismymom@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'charlie08',
+    email: 'charlesincharge@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'molly01',
+    email: 'mollyoutdoors@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'zoe0002',
+    email: 'zoedoesntexist@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'peter0003',
+    email: 'peteriswill@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'arcie30',
+    email: 'idonthaveabirthday@gmail.com',
+    password: 'password123'
+  },
+  {
+    username: 'willemma004',
+    email: 'momwantsus@gmail.com',
+    password: 'password123'
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
